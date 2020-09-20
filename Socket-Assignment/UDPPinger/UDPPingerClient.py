e242a6e5a21da6fd2c09eec15bbb436cc93a8bb4
dataisland <dataisland@outlook.com>
2020-09-20 15:32:18 +0800

    Update UDPPingerClient.py

--- Socket-Assignment/UDPPinger/UDPPingerClient.py
+++ Socket-Assignment/UDPPinger/UDPPingerClient.py
@@ -4,6 +4,7 @@ from socket import *
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(1)
 
+rtt_list = []
 for _ in range(10):
     data = b'This is a ping message'
     ticks = time.time()
@@ -11,6 +12,14 @@ for _ in range(10):
 
     try:        
         recvdata = clientSocket.recv(1024)
-        print('%s %.4fs' % (recvdata.decode(), time.time() - ticks))
+        rtt = time.time() - ticks
+        rtt_list.append(rtt)
+        print('Ping %d %.6fs' % (_, rtt))
+        print(recvdata.decode())
     except timeout:
         print('Request timed out')
+
+print(f'min rtt {min(rtt_list):.6f}s')
+print(f'max rtt {max(rtt_list):.6f}s')
+print(f'avg rtt {sum(rtt_list)/len(rtt_list):.6f}s')
+print(f'loss rate {100-10*len(rtt_list)}%')
