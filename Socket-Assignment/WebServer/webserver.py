731b7a01f14d8bb190a07aedd52ffc849b1d882d
dataisland <dataisland@outlook.com>
2020-09-20 15:23:11 +0800

    Muti thread webserver

--- Socket-Assignment/WebServer/webserver.py
+++ Socket-Assignment/WebServer/webserver.py
@@ -1,4 +1,7 @@
 #import socket module
+
+import time
+import threading
 from socket import *
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -18,30 +21,42 @@ Content-Type: text/html
 
 '''
 
+class ConnectionThread(threading.Thread):
+    def __init__(self, con):
+        super().__init__()
+        self.con = con
+
+    def run(self):
+        try:
+            message = self.con.recv(1024)
+            filename = message.split()[1]
+
+            if filename[1:] == b'sleep':
+                time.sleep(5)
+                self.con.sendall(header_200_ok)    
+                self.con.sendall(b'sleep')
+            else:
+                with open(filename[1:], 'rb') as f:
+                    outputdata = f.read()
+            
+                #Send one HTTP header line into socket
+                self.con.sendall(header_200_ok)    
+                
+                #Send the content of the requested file to the client
+                self.con.sendall(outputdata)
+            self.con.close()
+        except IOError:
+            #Send response message for file not found
+            self.con.sendall(header_404_notfound)
+            self.con.sendall(b'404 not found')
+            self.con.close()
+
 while True:
     #Establish the connection
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
+    
+    connectionThread = ConnectionThread(connectionSocket)
+    connectionThread.start()
 
-    try:
-        message = connectionSocket.recv(1024)
-        filename = message.split()[1]
-        f = open(filename[1:], 'rb')
-        outputdata = f.read()
-
-        #Send one HTTP header line into socket
-        connectionSocket.sendall(header_200_ok)
-
-        #Send the content of the requested file to the client
-        connectionSocket.sendall(outputdata)
-        connectionSocket.close()
-    except IOError:
-        #Send response message for file not found
-        connectionSocket.sendall(header_404_notfound)
-        connectionSocket.sendall(b'404 not found')
-        connectionSocket.close()
-        
-    except:        
-        connectionSocket.close()
-        
 serverSocket.close()
