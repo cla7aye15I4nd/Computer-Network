2aaeb926c27148dbd0ec5c688f456ae57134a41b
dataisland <dataisland@outlook.com>
2020-09-24 19:43:46 +0800

    Send image success

--- Socket-Assignment/SMTP/SMTPClient.py
+++ Socket-Assignment/SMTP/SMTPClient.py
@@ -2,56 +2,80 @@ import base64
 from socket import *
 from config import *
 
-subject = b'Subject: SMTP Lab\r\n\r\n'
-msg = b'\r\n I love computer networks!'
-endmsg = b'\r\n.\r\n'
-
-# Choose a mail server (e.g. Google mail server) and call it mailserver
 mailserver = ('smtp.qq.com', 25)
 
-# Create socket called clientSocket and establish a TCP connection with mailserver
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect(mailserver)
 
+# Verify 220
 recv = clientSocket.recv(1024).decode()
-
-print(recv)
-
 if recv[:3] != '220':
     print('220 reply not received from server.')
 
-# Send HELO command and print server response.
-heloCommand = b'EHLO Alice\r\n'
-clientSocket.send(heloCommand)
-
-recv1 = clientSocket.recv(1024).decode()
-print(recv1)
-
-if recv1[:3] != '250':
+# Verify 250
+clientSocket.send(b'EHLO Alice\r\n')
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '250':
     print('250 reply not received from server.')
 
-authCommand = b'AUTH PLAIN ' + base64.b64encode(b'\x00' + username + b'\x00' + password) + b'\r\n'
+# Login
+authCommand = b'AUTH PLAIN ' + base64.b64encode(b'\x00'+username+b'\x00'+password) + b'\r\n'
 clientSocket.send(authCommand)
 print(clientSocket.recv(1024).decode())
 
-# Send MAIL FROM command and print server response.
 clientSocket.send(b'MAIL FROM:<1982127876@qq.com>\r\n')
 print(clientSocket.recv(1024).decode())
 
-# Send RCPT TO command and print server response.
-clientSocket.send(b'RCPT TO:<1982127876@qq.com>\r\n')
+clientSocket.send(b'RCPT TO:<dataisland@sjtu.edu.cn>\r\n')
 print(clientSocket.recv(1024).decode())
 
-# Send DATA command and print server response.
 clientSocket.send(b'DATA\r\n')
 print(clientSocket.recv(1024).decode())
 
-# Send message data.
-clientSocket.send(subject)
-clientSocket.send(msg)
+subject = 'SUBJECT: SMTP Lab'
+meta = f'''
+Content-Type: multipart/alternative;boundary="BOUNDARY"
+Mime-Version: 1.0
+'''
 
-# Message ends with a single period.
-clientSocket.send(endmsg)
+message = f'''
+--BOUNDARY
+Content-type: text/html;
+charset="utf-8"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+
+<!DOCTYPE html>
+<html>
+<body>
+  <h1> Welcome using SMTP Client!</h1>
+  <p> If you see this page, the SMTP Client is successfully installed. </p>
+</body>
+</html>
+
+'''
+
+with open('avatar.png', 'rb') as f:
+    content = base64.b64encode(f.read()).decode()
+attachment = f'''
+--BOUNDARY
+Content-Type: application/octet-stream;
+Content-Disposition: attachment; filename="avatar.png"
+Content-Transfer-Encoding: base64
+
+{content}
+
+'''
+
+endmark  = b'--BOUNDARY--\r\n.\r\n'
+
+clientSocket.send(subject.encode())
+clientSocket.send(meta.encode())
+clientSocket.send(message.encode())
+clientSocket.send(attachment.encode())
+
+clientSocket.send(endmark)
 print(clientSocket.recv(1024).decode())
 
 # Send QUIT command and get server response.
