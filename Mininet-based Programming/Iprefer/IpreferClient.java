bac2a0d5d3828acff1da054cbc1753c1b68a268c
dataisland <dataisland@outlook.com>
2020-11-03 23:50:34 +0800

    More precise counter way

--- Mininet-based Programming/Iprefer/IpreferClient.java
+++ Mininet-based Programming/Iprefer/IpreferClient.java
@@ -21,18 +21,21 @@ public class IpreferClient{
             byte[] packet= new byte[1000];
 
             long total = (long) (this.time * 1000000000);
-            long save = System.nanoTime();            
+            long save = System.nanoTime(), realtotal;            
             
             int counter = 0;
-            while (System.nanoTime() - save < total) {
+            while (true) {
                 out.write(packet, 0, packet.length);
                 counter++;
+		        realtotal = System.nanoTime() - save;
+		        if (realtotal >= total)
+		            break;
             }            
             
             out.close();
-            socket.close();	
-            
-            double rate = counter * 8 / 1000 / this.time;
+            socket.close();
+
+            double rate = counter * 8 / 1000 / (realtotal / 1000000000.0);
             System.out.printf("sent=%dKB rate=%f Mbps\n", counter, rate);
         } catch (IOException e) {
             System.out.println("IpreferClient run error.");
