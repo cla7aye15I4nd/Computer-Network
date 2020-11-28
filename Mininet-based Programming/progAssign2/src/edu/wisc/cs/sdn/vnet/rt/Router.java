cca3570dce9bac2884e0a48f9551ed001787d1b3
dataisland <dataisland@outlook.com>
2020-11-28 16:38:35 +0800

    Modify Router and Switch Code

--- Mininet-based Programming/progAssign2/src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ Mininet-based Programming/progAssign2/src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -5,87 +5,106 @@ import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
 
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPv4;
 
 /**
  * @author Aaron Gember-Jacobson and Anubhavnidhi Abhashkumar
  */
 public class Router extends Device
 {	
-	/** Routing table for the router */
-	private RouteTable routeTable;
-	
-	/** ARP cache for the router */
-	private ArpCache arpCache;
-	
-	/**
-	 * Creates a router for a specific host.
-	 * @param host hostname for the router
-	 */
-	public Router(String host, DumpFile logfile)
-	{
-		super(host,logfile);
-		this.routeTable = new RouteTable();
-		this.arpCache = new ArpCache();
+    /** Routing table for the router */
+    private RouteTable routeTable;
+    
+    /** ARP cache for the router */
+    private ArpCache arpCache;
+    
+    /**
+     * Creates a router for a specific host.
+     * @param host hostname for the router
+     */
+    public Router(String host, DumpFile logfile)
+    {
+	super(host,logfile);
+	this.routeTable = new RouteTable();
+	this.arpCache = new ArpCache();
+    }
+    
+    /**
+     * @return routing table for the router
+     */
+    public RouteTable getRouteTable()
+    { return this.routeTable; }
+    
+    /**
+     * Load a new routing table from a file.
+     * @param routeTableFile the name of the file containing the routing table
+     */
+    public void loadRouteTable(String routeTableFile)
+    {
+	if (!routeTable.load(routeTableFile, this)) {
+	    System.err.println("Error setting up routing table from file "
+			       + routeTableFile);
+	    System.exit(1);
 	}
 	
-	/**
-	 * @return routing table for the router
-	 */
-	public RouteTable getRouteTable()
-	{ return this.routeTable; }
-	
-	/**
-	 * Load a new routing table from a file.
-	 * @param routeTableFile the name of the file containing the routing table
-	 */
-	public void loadRouteTable(String routeTableFile)
-	{
-		if (!routeTable.load(routeTableFile, this))
-		{
-			System.err.println("Error setting up routing table from file "
-					+ routeTableFile);
-			System.exit(1);
-		}
-		
-		System.out.println("Loaded static route table");
-		System.out.println("-------------------------------------------------");
-		System.out.print(this.routeTable.toString());
-		System.out.println("-------------------------------------------------");
+	System.out.println("Loaded static route table");
+	System.out.println("-------------------------------------------------");
+	System.out.print(this.routeTable.toString());
+	System.out.println("-------------------------------------------------");
+    }
+    
+    /**
+     * Load a new ARP cache from a file.
+     * @param arpCacheFile the name of the file containing the ARP cache
+     */
+    public void loadArpCache(String arpCacheFile)
+    {
+	if (!arpCache.load(arpCacheFile)) {
+	    System.err.println("Error setting up ARP cache from file "
+			       + arpCacheFile);
+	    System.exit(1);
 	}
 	
-	/**
-	 * Load a new ARP cache from a file.
-	 * @param arpCacheFile the name of the file containing the ARP cache
-	 */
-	public void loadArpCache(String arpCacheFile)
-	{
-		if (!arpCache.load(arpCacheFile))
-		{
-			System.err.println("Error setting up ARP cache from file "
-					+ arpCacheFile);
-			System.exit(1);
-		}
-		
-		System.out.println("Loaded static ARP cache");
-		System.out.println("----------------------------------");
-		System.out.print(this.arpCache.toString());
-		System.out.println("----------------------------------");
-	}
+	System.out.println("Loaded static ARP cache");
+	System.out.println("----------------------------------");
+	System.out.print(this.arpCache.toString());
+	System.out.println("----------------------------------");
+    }
+    
+    /**
+     * Handle an Ethernet packet received on a specific interface.
+     * @param etherPacket the Ethernet packet that was received
+     * @param inIface the interface on which the packet was received
+     */
+    public void handlePacket(Ethernet etherPacket, Iface inIface)
+    {
+	System.out.println("*** -> Received packet: " +
+			   etherPacket.toString().replace("\n", "\n\t"));
+	
+	if (etherPacket.getEtherType() != Ethernet.TYPE_IPv4) return;
+	
+	IPv4 payload = (IPv4) etherPacket.getPayload();
+	
+	short checksum = payload.getChecksum();
+	byte TTL = payload.getTtl();
+	
+	payload.serialize();
+	if (payload.getChecksum() != checksum) return;
+	if (--TTL == 0) return;
+	
+	payload.setTtl(TTL);	
+	for (Iface iface : this.interfaces.values())
+	    if (iface.getIpAddress() == payload.getDestinationAddress())
+		return;
 
-	/**
-	 * Handle an Ethernet packet received on a specific interface.
-	 * @param etherPacket the Ethernet packet that was received
-	 * @param inIface the interface on which the packet was received
-	 */
-	public void handlePacket(Ethernet etherPacket, Iface inIface)
-	{
-		System.out.println("*** -> Received packet: " +
-                etherPacket.toString().replace("\n", "\n\t"));
-		
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
-		
-		
-		/********************************************************************/
-	}
+	RouteEntry nexthop = this.routeTable.lookup(payload.getDestinationAddress());
+	if (nexthop == null || nexthop.getInterface().getName().equals(inIface.getName())) return;
+
+	etherPacket.setSourceMACAddress(nexthop.getInterface().getMacAddress().toString());
+	etherPacket.setDestinationMACAddress(this.arpCache.lookup(nexthop.getGatewayAddress()).getMac().toString());
+
+	payload.serialize();
+	etherPacket.setPayload(payload);
+	sendPacket(etherPacket, nexthop.getInterface());
+    }
 }
--- Mininet-based Programming/progAssign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ Mininet-based Programming/progAssign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -1,37 +1,78 @@
 package edu.wisc.cs.sdn.vnet.sw;
 
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.MACAddress;
+
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
+
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * @author Aaron Gember-Jacobson
  */
 public class Switch extends Device
 {	
-	/**
-	 * Creates a router for a specific host.
-	 * @param host hostname for the router
-	 */
-	public Switch(String host, DumpFile logfile)
-	{
-		super(host,logfile);
+    /**
+     * Creates a router for a specific host.
+     * @param host hostname for the router
+     */
+    public Switch(String host, DumpFile logfile)
+    {
+	super(host,logfile);
+    }
+    
+    /**
+     * Handle an Ethernet packet received on a specific interface.
+     * @param etherPacket the Ethernet packet that was received
+     * @param inIface the interface on which the packet was received
+     */
+    public void handlePacket(Ethernet etherPacket, Iface inIface)
+    {
+	System.out.println("*** -> Received packet: " +
+			   etherPacket.toString().replace("\n", "\n\t"));
+	
+	
+	for (Map.Entry<MACAddress, Entry> entry : switching_table.entrySet()) {
+	    if (System.currentTimeMillis() - entry.getValue().timestamp > TIME_LIMIT)
+		switching_table.remove(entry.getKey());
 	}
 
-	/**
-	 * Handle an Ethernet packet received on a specific interface.
-	 * @param etherPacket the Ethernet packet that was received
-	 * @param inIface the interface on which the packet was received
-	 */
-	public void handlePacket(Ethernet etherPacket, Iface inIface)
-	{
-		System.out.println("*** -> Received packet: " +
-                etherPacket.toString().replace("\n", "\n\t"));
-		
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
-		
-		/********************************************************************/
+	Entry src = switching_table.get(etherPacket.getSourceMAC());
+	Entry dst = switching_table.get(etherPacket.getDestinationMAC());
+
+	if (src == null) 
+	    switching_table.put(etherPacket.getSourceMAC(), new Entry(inIface.getName()));
+	else
+	    src.timestamp = System.currentTimeMillis();
+
+	if (dst == null) {
+	    for (Iface iface: interfaces.values()) {
+		if (!iface.getName().equals(inIface.getName()))
+		    super.sendPacket(etherPacket, iface);
+	    }
+	} else
+	    super.sendPacket(etherPacket, interfaces.get(dst.iface));
+	    
+    }
+    
+    private final long TIME_LIMIT = 15000;
+    private HashMap<MACAddress, Entry> switching_table = new HashMap<MACAddress, Entry> ();
+
+    private class Entry{
+	String iface;
+	long timestamp;
+	
+	Entry (String iface) {
+	    this.iface = iface;
+	    this.timestamp = System.currentTimeMillis();
 	}
+
+	@Override
+	public String toString() {
+	    return String.format("Entry(%s, %d)", iface, timestamp);
+	}
+    };
 }
